71b8591ffb04d9f059794a8abcead32beee619a2
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-12-11 12:21:07 +0200

    Add repo & pull count summaries
    Closes #136

--- api/repos/[username].ts
+++ api/repos/[username].ts
@@ -1,6 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { getPullRequestsByUser, groupPullRequestsByRepository } from 'goodbrother';
 import { authenticate } from '../../src/auth';
+import { summarizeRepos } from '../../src/summary';
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   const { username, secret } = request.query;
@@ -9,8 +10,9 @@ export default async (request: VercelRequest, response: VercelResponse) => {
     if (typeof username === 'string') {
       const openPRs = await getPullRequestsByUser(username);
       const grouped = groupPullRequestsByRepository(openPRs);
+      const summary = summarizeRepos(grouped, openPRs);
 
-      response.status(200).send(grouped);
+      response.status(200).send(summary);
     }
   } else {
     response.status(401).send('Unauthorized. Provide "secret" as query param');
